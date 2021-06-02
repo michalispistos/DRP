260ae37887534c9e9cc9ceff1395ff7771ef9018
ss8919 <ss8919@ic.ac.uk>
2021-06-03 00:01:45 +0100

    [mp2019][ss8919][crl19][mb2319] Updated project tiles to use project information from database

--- frontend/src/components/projectTileGrid.js
+++ frontend/src/components/projectTileGrid.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ProjectTile from './projectTile';
 
 class ProjectTileGrid extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: []
+        }
+        this.getProjects();
+    }
 
     renderTile(title, lookingFor) {
         return <ProjectTile title={title} lookingFor={lookingFor} />;
     }
-
+    
+    getProjects = async () => {
+        try {
+          const response = await fetch("https://drp12-backend.herokuapp.com/projects");
+          const jsonData = await response.json();
+    
+          this.setState({projects: jsonData})
+     
+        } catch (err) {
+          console.error(err.message);
+        }
+      };
       
     render() {
         return (
         <div class="projectTileGrid">
-            {this.renderTile("title1", "a computing student")}
-            {this.renderTile("title2", "a computing student")}
-            {this.renderTile("title3", "a computing student")}
+            {this.state.projects.map(project => this.renderTile(project.title,project.looking_for))}
         </div>
         );
     }
