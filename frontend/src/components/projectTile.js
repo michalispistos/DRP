14eecef0a4f7d4114fc62dc0baac0f4d2c3e0934
ss8919 <ss8919@ic.ac.uk>
2021-06-02 20:17:00 +0100

    [mp2019][crl19][mb2319][ss8919] Fixed all warnings

--- frontend/src/components/projectTile.js
+++ frontend/src/components/projectTile.js
@@ -14,7 +14,7 @@ class ProjectTile extends React.Component {
     render() { 
         return ( 
             <div className="projectTile">
-                <img className="projectTileImage" src={"images/"+this.state.imageSrc} alt={"project image"}/>
+                <img className="projectTileImage" src={"images/"+this.state.imageSrc} alt={"guy studying"}/>
                 <h4 className="projectTileTitle">{this.state.title}</h4>
                 <h4 className="projectTileLookingFor">{this.state.lookingFor}</h4>
             </div>
