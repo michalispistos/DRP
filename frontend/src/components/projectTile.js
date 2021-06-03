8ca1215bece37c885d426b5c1392ab0efb52fc65
ss8919 <ss8919@ic.ac.uk>
2021-06-03 16:32:17 +0100

    [mp2019][ss8919][crl19][mb2319] Changed opacity of project image and modified gitlab yml

--- frontend/src/components/projectTile.js
+++ frontend/src/components/projectTile.js
@@ -16,7 +16,7 @@ class ProjectTile extends React.Component {
     render() { 
         const styles = {
             projectTile: {
-              backgroundImage: `linear-gradient(rgba(255,255,255,0.6), rgba(255,255,255,0.6)), url(${this.state.imageSrc})`,
+              backgroundImage: `linear-gradient(rgba(255,255,255,0.4), rgba(255,255,255,0.4)), url(${this.state.imageSrc})`,
               backgroundPosition: 'center',
               backgroundRepeat: 'no-repeat',
               backgroundSize: 'cover',
